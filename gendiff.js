06e707d0e7a840cd97e1f277e477715ba7cc793b
kolix2006 <kolyanychkolya5@gmail.com>
2026-03-09 13:15:53 +0300

    gendiff modified

--- gendiff.js
+++ gendiff.js
@@ -8,5 +8,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference')
   .version('1.0.0')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse()
