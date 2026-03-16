e47fcc8a18b62d1dd572f97428e74be4d317cf78
kolix2006 <kolyanychkolya5@gmail.com>
2026-03-16 12:33:37 +0300

    test-coverage fixed

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,10 +11,6 @@ const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
 
 describe('Проверка работоспособности проекта', () => {
   test('Сравнение плоских JSON-файлов', () => {
-    // const received = genDiffs(getFixturePath('file1.json'), getFixturePath('file2.json'))
-    // const expected = readFile('expectedFile.txt').trim()
-    // console.log(JSON.stringify(received))
-    // console.log(JSON.stringify(expected))
     expect(genDiffs(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(readFile('expectedFile.txt').trim())
   })
 })
